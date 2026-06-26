dbbd384d8cedf595dd5754179803f22a211e190c
eumditravel-oss <eumditravel@gmail.com>
2026-06-26 18:34:54 +0900

    Make scroll animations repeatable on scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -747,7 +747,8 @@ const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
-      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove('visible');
     }
   });
 }, observerOptions);
